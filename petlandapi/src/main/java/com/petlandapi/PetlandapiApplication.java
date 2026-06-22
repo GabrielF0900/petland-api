037c96d288ae8685a59fcf62610c6c15635dda1e
GabrielF0900 <gabrielcfonline0900@gmail.com>
2026-06-22 17:01:07 -0300

    INICIO DE TUDO: Enviando estrutura base do projeto. Data 22/06/2026

---
+++ petlandapi/src/main/java/com/petlandapi/PetlandapiApplication.java
@@ -0,0 +1,13 @@
+package com.petlandapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetlandapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetlandapiApplication.class, args);
+	}
+
+}
---
+++ petlandapi/src/test/java/com/petlandapi/PetlandapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petlandapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetlandapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
